48516a5adfcb97042f14cefc928c1d6ad8c84924
alfaarghya <alfaarghya.dev@gmail.com>
2025-02-21 00:19:18 +0530

    📣 update: lint check

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,7 @@ import startServer from "./helper/startServer";
 
 dotenv.config();
 
+// eslint-disable-next-line turbo/no-undeclared-env-vars
 const port = process.env.PORT || 8080; //server port
 
 //start the server
