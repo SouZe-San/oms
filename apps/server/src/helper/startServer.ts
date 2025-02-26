558d86ec9c8284ed72313c971e6d985b038b4641
alfaarghya <alfaarghya.dev@gmail.com>
2025-02-26 23:36:02 +0530

    server: connect db

--- apps/server/src/helper/startServer.ts
+++ apps/server/src/helper/startServer.ts
@@ -1,9 +1,9 @@
-// import dbConnect from "./dbConnect";
+import dbConnect from "./dbConnect";
 
 const startServer = async (port: number) => {
   try {
     console.log("starting server....");
-    // await dbConnect(); //connect to database
+    await dbConnect(); //connect to database
     console.log(`Server is running on http://localhost:${port} ⚙️`);
   } catch (err) {
     console.log("❌ Error while starting server : " + err);
