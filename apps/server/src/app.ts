62929a56531a69fd6b80aaa73b404bcc622f2ecd
ImanSamantaCoder <iman.samanta.code@gmail.com>
2025-03-14 20:23:49 +0530

    ⚙️ server: check routes middleware

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import checkRoutes from "./middleware/checkRoutes";
 
 const app = express();
 app.use(express.json()); //parse incoming JSON
+
+app.use(checkRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "hello from OBT server" });
