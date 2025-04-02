2ffe1f05bac92c9311e1b53843f0d35564aed15e
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-04-02 10:38:18 +0530

    ⚙️server: little cleaning 🧹

--- packages/types/src/validators/product.validator.ts
+++ packages/types/src/validators/product.validator.ts
@@ -1,17 +1,19 @@
 import { z } from "zod";
 
+// @alfaarghya
 //Validate create product request
 export const createProductValidator = z.object({
   adminId: z.string().uuid().nonempty("User ID is required"),
   name: z.string().nonempty("Product Name is required"),
   description: z.string().optional(),
-  price: z.number()
+  price: z
+    .number()
     .min(0.01, "Price must be greater than 0")
     .refine((value) => /^\d+(\.\d{1,2})?$/.test(value.toFixed(2)), {
       message: "Price must have at most 2 decimal places",
     })
     .transform((value) => parseFloat(value.toFixed(2))), // Ensures 2 decimal places
-  stock: z.number().int().positive("Stock must be positive value")
+  stock: z.number().int().positive("Stock must be positive value"),
 });
 
 // Validate get products request by adminId
@@ -28,7 +30,8 @@ export const updateProductValidator = z.object({
   updateName: z.string().optional(),
   updateDescription: z.string().optional(),
   updateStock: z.number().int().positive("Stock must be positive value").optional(),
-  updatePrice: z.number()
+  updatePrice: z
+    .number()
     .min(0.01, "Price must be greater than 0")
     .refine((value) => /^\d+(\.\d{1,2})?$/.test(value.toFixed(2)), {
       message: "Price must have at most 2 decimal places",
@@ -41,10 +44,12 @@ export const updateProductValidator = z.object({
 export const deleteProductValidator = z.object({
   adminId: z.string().uuid().nonempty("User ID is required"),
   productId: z.string().uuid().nonempty("Product ID is required"),
-})
+});
 
 // ! CUSTOMER - PART
 
+// @SouZe-San
+// Validate get product request
 export const getProduct_reqSchema = z.object({
   skipCount: z.number().int().nonnegative("skipCount should be >= 0").default(0).optional(),
   takeCount: z.number().int().positive("takeCount must be positive").default(10).optional(),
