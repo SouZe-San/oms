0553f4a4c2a805ca7284cdc6ea3a4fc75a44bb9a
alfaarghya <alfaarghya.dev@gmail.com>
2025-05-13 17:12:46 +0530

    🧑‍💻 sites: add sign up page

---
+++ apps/admin/src/app/auth/signup/page.tsx
@@ -0,0 +1,8 @@
+import { Role } from "@oms/types/user.type"
+import SignupPage from "@oms/ui/pages/SignupPage"
+
+const Page = () => {
+  return <SignupPage role={Role.ADMIN} />
+}
+
+export default Page;
