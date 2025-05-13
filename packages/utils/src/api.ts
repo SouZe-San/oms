6d4b4d6b3f253d9eccf64a162b9ee9acba1c89fd
alfaarghya <alfaarghya.dev@gmail.com>
2025-05-13 16:57:09 +0530

    📦 utils: update

--- packages/utils/src/api.ts
+++ packages/utils/src/api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  baseURL: process.env.DEV_API,
-  // baseURL: process.env.PRODUCTION_API, //production api
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api',
   withCredentials: true,
 });
 
-export default API;
+export default api;
