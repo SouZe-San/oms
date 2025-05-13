0a073d77f861c78dff83d5f060a88d4f651ed7af
alfaarghya <alfaarghya.dev@gmail.com>
2025-05-13 17:11:53 +0530

    🧑‍💻 sites: add sign in page

---
+++ apps/user/src/app/auth/signin/page.tsx
@@ -0,0 +1,8 @@
+import { Role } from "@oms/types/user.type";
+import SigninPage from "@oms/ui/pages/SigninPage";
+
+const Page = () => {
+  return <SigninPage role={Role.CUSTOMER} />
+}
+
+export default Page;
