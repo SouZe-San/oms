d324ab9fd1366db9410e9866dd1da99b13aca6ef
alfaarghya <alfaarghya.dev@gmail.com>
2025-05-13 17:15:28 +0530

    🛠️ Fix: TypeError => __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$react$2d$redux$2f$dist$2f$rsc$2e$mjs__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__.useDispatch.withTypes is not a function

--- packages/store/src/hooks.ts
+++ packages/store/src/hooks.ts
@@ -1,7 +1,6 @@
-import { useDispatch, useStore, useSelector } from "react-redux";
+import { useDispatch, useStore, useSelector, TypedUseSelectorHook } from "react-redux";
 import { AppDispatch, RootState, AppStore } from "./store";
 
-// use this Hooks instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore: () => AppStore = useStore;
