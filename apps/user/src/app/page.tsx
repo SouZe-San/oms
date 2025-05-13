48df8af75ff9513c0de75ab3426c6bf2232aff2e
alfaarghya <alfaarghya.dev@gmail.com>
2025-05-13 17:18:06 +0530

    🧹 remove: examples & old files

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -1,23 +1,8 @@
-import { Button } from "@oms/ui/button";
-import { CountButton } from "@oms/ui/countButton";
-import TempoCount from "../components/TempoCount";
-
 const Home = () => {
   return (
     <div className="">
       <h1 className="text-2xl bg-blue-400 text-center">User</h1>
-      <div className="flex justify-center mt-5">
-        <Button appName="user" className="bg-black text-xl text-amber-50 p-2 rounded-2xl cursor-pointer">
-          Open alert
-        </Button>
-      </div>
 
-      <div className="flex justify-center mt-16 gap-5 items-center">
-        <TempoCount />
-        <CountButton className="">
-          Upgrade
-        </CountButton>
-      </div>
     </div>
   );
 };
