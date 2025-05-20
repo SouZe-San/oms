003c3f1056d8241ae333b9cae5d2c4ac193de1e0
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-05-20 19:37:39 +0530

    About section : Layout added

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@oms/ui/components/home/hero-section/HeroSection";
+import AboutSection from "@oms/ui/components/home/about-section/AboutSection";
 import NavBar from "@oms/ui/components/nav/NavBar";
 const Home = () => {
   return (
     <>
       <NavBar />
-      <main>
+      <main className="">
         <HeroSection />
+        <AboutSection />
       </main>
     </>
   );
