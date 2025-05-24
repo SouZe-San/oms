2094496d1d85812e55224411434a6e0151225e32
alfaarghya <alfaarghya.dev@gmail.com>
2025-05-24 19:24:06 +0530

    🧑‍💻 admin site: connect dashboard navbar

--- apps/admin/src/app/dashboard/layout.tsx
+++ apps/admin/src/app/dashboard/layout.tsx
@@ -1,13 +1,18 @@
+import { Role } from "@oms/types/user.type";
 import Sidebar from "@oms/ui/components/admin/Sidebar";
+import DashboardNav from "@oms/ui/components/nav/dashboard-nav";
 import { ReactNode } from "react";
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex w-full h-screen">
-      <Sidebar />
-      <main className="flex-1 bg-white overflow-y-auto">
+    <>
+      <DashboardNav role={Role.ADMIN} />
+      <div className="fixed top-20 left-0 h-[calc(100vh-5rem)] w-64 bg-white border-r z-10">
+        <Sidebar />
+      </div>
+      <main className="ml-64 mt-20 h-[calc(100vh-5rem)] overflow-y-auto p-4 bg-white">
         {children}
       </main>
-    </div>
+    </>
   );
 }
