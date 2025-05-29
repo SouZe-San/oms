570482c2be290bf6af55249261dbb8e5251fbb60
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-05-29 23:01:33 +0530

    UI : Product Sot based on Category 📢🎢

--- packages/ui/src/components/customer/products/CategoryBtn.tsx
+++ packages/ui/src/components/customer/products/CategoryBtn.tsx
@@ -1,7 +1,24 @@
 import { ProductCategory } from "@oms/types/product.type";
 import "./style.css";
-const CategoryBtn = ({ title }: { title: ProductCategory }) => {
-  return <button className="category-btn font-neue text-xl snap-start ">{title}</button>;
+import { Dispatch, SetStateAction } from "react";
+const CategoryBtn = ({
+  title,
+  setCategory,
+  category,
+}: {
+  title: ProductCategory;
+  setCategory: Dispatch<SetStateAction<ProductCategory | null>>;
+  category?: ProductCategory | null;
+}) => {
+  console.log("CategoryBtn rendered with title:", title, "and category:", category);
+  return (
+    <button
+      className={`category-btn font-neue text-sm snap-start ${category === title ? "active" : ""}`}
+      onClick={() => setCategory(title)}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default CategoryBtn;
--- packages/ui/src/pages/customer/home/Home.tsx
+++ packages/ui/src/pages/customer/home/Home.tsx
@@ -1,10 +1,17 @@
+"use client";
 import ProductContainer from "../../../components/customer/products/product-container";
 import SearchSection from "../../../components/customer/search-section/SearchSection";
 import { ProductsResponse } from "@oms/types/api.type";
 import { getProducts } from "@oms/utils/api.customer";
 import { axiosErrorHandler } from "@oms/utils/handlers";
 import CategoryBtn from "../../../components/customer/products/CategoryBtn";
-const Home = async () => {
+import { ProductCategory } from "@oms/types/product.type";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [category, setCategory] = useState<ProductCategory | null>(null);
+  const [nonFilteredProducts, setNONFilterProduct] = useState<ProductsResponse[]>([]);
+  const [products, setProducts] = useState<ProductsResponse[]>([]);
   const fetchProducts = async () => {
     try {
       const { data } = await getProducts();
@@ -16,33 +23,44 @@ const Home = async () => {
     }
   };
 
-  const products: ProductsResponse[] = await fetchProducts();
-
+  useEffect(() => {
+    (async () => {
+      const products: ProductsResponse[] = await fetchProducts();
+      setProducts(products);
+      setNONFilterProduct(products);
+    })();
+  }, []);
   const productCategories = [...new Set(products.map((product) => product.category))];
+
+  useEffect(() => {
+    if (!category) {
+      setProducts(nonFilteredProducts);
+      return;
+    }
+    const filteredProducts = products.filter((product) => product.category === category);
+    setProducts(filteredProducts);
+  }, [category, products, nonFilteredProducts]);
 
   return (
     <section>
       <SearchSection />
 
-      <div className="flex overflow-hidden justify-center items-center mt-4 mb-4 md:px-16">
+      <div className="flex overflow-hidden justify-start items-center mt-4 mb-4 md:px-16">
         <div
-          className="flex overflow-x-auto gap-2 justify-start items-center w-full scroll-auto snap-x category-btn-section"
+          className="flex overflow-x-auto gap-2 justify-start items-center scroll-auto snap-x category-btn-section"
           style={{
             scrollbarWidth: "none",
             msOverflowStyle: "none",
             WebkitOverflowScrolling: "touch",
           }}
         >
+          <button className="category-btn font-neue text-xl snap-start " onClick={() => setCategory(null)}>
+            Clear x
+          </button>
           {productCategories.length > 0 &&
-            productCategories.map((category, index) => <CategoryBtn key={index.toString()} title={category} />)}
-          <CategoryBtn title={"APPLIANCES"} />
-          <CategoryBtn title={"APPLIANCES"} />
-          <CategoryBtn title={"APPLIANCES"} />
-          <CategoryBtn title={"APPLIANCES"} />
-          <CategoryBtn title={"APPLIANCES"} />
-          <CategoryBtn title={"APPLIANCES"} />
-          <CategoryBtn title={"APPLIANCES"} />
-          <CategoryBtn title={"APPLIANCES"} />
+            productCategories.map((categoryName, index) => (
+              <CategoryBtn key={index.toString()} title={categoryName} setCategory={setCategory} category={category} />
+            ))}
         </div>
       </div>
 
