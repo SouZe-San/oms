30bc5fc9dd4936ad1367f75555e0ba8b74d82b9c
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-05-29 20:38:08 +0530

    🍥Product : Height of Every Card in Same Alignment

--- packages/ui/src/components/customer/products/ProductCard.tsx
+++ packages/ui/src/components/customer/products/ProductCard.tsx
@@ -8,8 +8,8 @@ import { ProductsResponse } from "@oms/types/api.type";
 
 const ProductCard = ({ product }: { product: ProductsResponse }) => {
   return (
-    <div className="border px-4 pt-3 pb-6 product-card md:w-[23%] max-w-[25rem]">
-      <Link href={`/product/${product.id}`}>
+    <div className="border px-4 pt-3 pb-6 product-card md:w-[23%] max-w-[25rem] 3xl:mr-0">
+      <Link href={`/product/${product.id}`} className="flex flex-col justify-between h-full">
         <div className="product-img rounded-lg">
           <Image
             src={product.imageUrl || logo}
@@ -24,7 +24,7 @@ const ProductCard = ({ product }: { product: ProductsResponse }) => {
           <p className="product-description">{product.description}</p>
           <span className="rating-stars text-yellow-200">★★★★☆ 4.0</span>
           <div className="flex justify-between items-center mt-2">
-            <span className="product-price text-xl">${product.price}</span>
+            <span className="product-price text-xl">&#x20B9; {product.price}</span>
             <button className="add-to-cart-btn flex items-center cursor-pointer">
               Add
               <Image src={cart} alt="Add to Cart" width={24} height={24} className="inline-block ml-2" />
