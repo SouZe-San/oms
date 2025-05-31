4b83e480d839431848c79848169274dd2eb96d3a
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-05-31 10:07:03 +0530

    Api: remove error.data {No ned to Show responseDate which is no Coming)

--- packages/utils/src/handlers.ts
+++ packages/utils/src/handlers.ts
@@ -3,10 +3,7 @@ import axios from "axios";
 export const axiosErrorHandler = (error: unknown, from: string) => {
   console.log(`\n${from}\n`);
   if (axios.isAxiosError(error)) {
-    if (error.response) {
-      console.error("Error Data:", error.response.data);
-    } else {
-      // Something happened in setting up the request that triggered an Error
+    if (error.message) {
       console.error("Error Message:", error.message);
     }
   } else {
